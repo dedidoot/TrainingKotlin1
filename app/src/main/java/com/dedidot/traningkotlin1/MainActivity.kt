bac3391219fb3aae24430a02298213a92f44c72e
Dedi Doot <dedidoot@gmail.com>
2018-08-28 05:36:07 -0700

    up

--- app/src/main/java/com/dedidot/traningkotlin1/MainActivity.kt
+++ app/src/main/java/com/dedidot/traningkotlin1/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item_view.*
 
 class MainActivity : AppCompatActivity() {
 
