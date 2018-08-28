5f58571a5af01839cb66635ea38f95195192ac03
Dedi Doot <dedidoot@gmail.com>
2018-08-28 04:03:40 -0700

    up

---
+++ app/src/main/java/com/dedidot/traningkotlin1/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.dedidot.traningkotlin1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
